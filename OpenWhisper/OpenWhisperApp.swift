98f66fa8b1f01ab2fff037d57a7ffada1d0f2a78
Jefersson Nathan <malukenho.dev@gmail.com>
2026-02-19 18:10:05 +0100

    Initial Commit

---
+++ OpenWhisper/OpenWhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWhisperApp.swift
+//  OpenWhisper
+//
+//  Created by Jefersson Nathan de Oliveira Chaves on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenWhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
